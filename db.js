2f96f232ff2dcdba582ca54d6494de9712f3fc3b
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-01 21:37:33 -0500

    deploying to heroku, testing phase

--- db.js
+++ db.js
@@ -11,3 +11,5 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 });
 
 module.exports = sequelize;
+
+// const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
