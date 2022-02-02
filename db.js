cb2a780cf6dd6257b2fbfb65d2c51dbd4d115cae
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-01 20:48:14 -0500

    test heroku

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-})
+    dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT === 'production'
+});
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// })
 
 // const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
 
