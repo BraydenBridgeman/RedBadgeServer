e93ddd2e6cbe87516c74b783565dc84e540061c3
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-01 22:13:09 -0500

    server pushed successfully to heroku, need to test in Postman

--- db.js
+++ db.js
@@ -12,4 +12,5 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 
 module.exports = sequelize;
 
+// local server for npm start
 // const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
