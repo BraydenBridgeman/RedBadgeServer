20eb963a36a6960e09469ce0f71f961e25f55c07
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-19 16:04:06 -0500

    debugging cors issue

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
-// const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize(`${process.env.DATABASE_URL}`);
-
-// module.exports = sequelize;
-
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+            },
+        },
 });
 
 module.exports = sequelize;
