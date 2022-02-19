b794186ca8a42d2c23a4073a5225da3592aaca19
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-19 15:45:29 -0500

    heroku logs

--- db.js
+++ db.js
@@ -8,12 +8,6 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-            },
-        },
 });
 
 module.exports = sequelize;
