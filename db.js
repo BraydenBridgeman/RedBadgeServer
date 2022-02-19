64839767e50b9762e563a888ec5713868849fb3d
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-18 20:50:01 -0500

    updated db.js

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, {
     dialect: "postgres",
     dialectOptions: {
         ssl: {
