a690bb24a71f5dfb7187136487450d49b56208de
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-18 21:11:23 -0500

    cors issue debuggin marathon

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.use(cors({origin: whiteList, credentials: true}));
 app.use(require('./middleware/headers'));
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//     res.header('access-control-allow-origin', '*');
-//     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
-//     res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+app.use((req, res, next) => {
+    res.header('access-control-allow-origin', '*');
+    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
+    res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-//     next();
-// });
+    next();
+});
 
 app.use('/commentReview', controllers.commentReview);
 app.use('/allMovies', controllers.getMovies);
