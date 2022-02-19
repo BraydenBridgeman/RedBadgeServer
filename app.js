e7f7314ca9ade05e889a5f21f14e4b3835c37785
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-18 20:56:50 -0500

    test cors issue

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 require("dotenv").config();
 
-const Express = require("express");
-const app = Express();
+const express = require("express");
+const app = express();
 
 const dbConnection = require('./db');
 const controllers = require('./controllers');
+const fetch = require('node-fetch');
+globalThis.fetch = fetch
+const cors = require('cors')
+
+let whiteList = ['http://localhost:3000', 'http://localhost:5000', 'https://bwb-redbadgemovie-client.herokuapp.com/']
+app.use(cors({origin: whiteList, credentials: true}));
 
 app.use(require('./middleware/headers'));
+app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header('access-control-allow-origin', '*');
-    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
-    res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+// app.use((req, res, next) => {
+//     res.header('access-control-allow-origin', '*');
+//     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
+//     res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-    next();
-});
-
-app.use(Express.json());
+//     next();
+// });
 
 app.use('/commentReview', controllers.commentReview);
 app.use('/allMovies', controllers.getMovies);
@@ -27,7 +32,7 @@ app.use('/userlogin', controllers.userLogin);
 dbConnection.authenticate()
     .then(()=> dbConnection.sync())
     .then(()=>{
-        app.listen(process.env.PORT, ()=> {
+        app.listen(process.env.PORT, () => {
             console.log(`[Server]: App is running on ${process.env.PORT}.`);
         });        
     })
